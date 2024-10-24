ec36c0616fe5423a3ac6a3f70c5a3c28d5f2cd2f
YORNsoksunnara <163433515+YORNsoksunnara@users.noreply.github.com>
2024-10-24 09:20:59 +0700

    TP1 finish step3

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+      This is my first vuejs project.
+      
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
